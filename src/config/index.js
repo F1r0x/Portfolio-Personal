bc3f2b21e6810cbaac327b6483638cacaa07dddb
F1r0x <patrick94schonewolf@gmail.com>
2025-11-30 06:38:44 +0100

    PRUEBA 7

--- src/config/index.js
+++ src/config/index.js
@@ -1,52 +1,32 @@
-/* eslint-disable max-len */
 import featuredProjects from './featured';
 import projects from './projects';
 
-module.exports = {
-  email: 'patrickschonewolf@hotmail.com',
-  featuredProjects,
-  projects,
-  skills: ['Linux', 'HTML', 'CSS', 'JavaScript', 'React', 'Next.js', 'Bash', 'TypeScript', 'Git', 'Docker', 'Virtualbox', 'Nmap', 'Metasploit', 'Wireshark', 'Burp Suite', 'WordPress', 'Elementor',  'MySQL', 'N8N', 'OpenAI API', 'Photoshop'],
-  socialMedia: [
-    {
-      name: 'GitHub',
-      url: 'https://github.com/f1r0x',
-    },
-    {
-      name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/f1r0x/',
-    },
-  ],
+export const email = 'patrickschonewolf@hotmail.com';
+export const featuredProjects = featuredProjects;
+export const projects = projects;
 
-  navLinks: [
-    {
-      name: 'Sobre Mí',
-      url: '/#about',
-    },
-    {
-      name: 'Proyectos',
-      url: '/#projects',
-    },
-    {
-      name: 'Experiencia',
-      url: '/#experience',
-    },
-    {      
-      name: 'Estudios',
-      url: '/#education',
-    },
-    {      
-      name: 'HTB',
-      url: '/#hackthebox',
-    },
-    {
-      name: 'Contacto',
-      url: '/#contact',
-    },
-  ],
-  colors: {
-    green: '#64ffda',
-    navy: '#0a192f',
-    darkNavy: '#020c1b',
-  },
+export const skills = [
+  'Linux', 'HTML', 'CSS', 'JavaScript', 'React', 'Next.js', 'Bash', 'TypeScript', 'Git', 
+  'Docker', 'Virtualbox', 'Nmap', 'Metasploit', 'Wireshark', 'Burp Suite', 'WordPress', 
+  'Elementor', 'MySQL', 'N8N', 'OpenAI API', 'Photoshop'
+];
+
+export const socialMedia = [
+  { name: 'GitHub', url: 'https://github.com/f1r0x' },
+  { name: 'Linkedin', url: 'https://www.linkedin.com/in/f1r0x/' },
+];
+
+export const navLinks = [
+  { name: 'Sobre Mí', url: '/#about' },
+  { name: 'Proyectos', url: '/#projects' },
+  { name: 'Experiencia', url: '/#experience' },
+  { name: 'Estudios', url: '/#education' },
+  { name: 'HTB', url: '/#hackthebox' },
+  { name: 'Contacto', url: '/#contact' },
+];
+
+export const colors = {
+  green: '#64ffda',
+  navy: '#0a192f',
+  darkNavy: '#020c1b',
 };
