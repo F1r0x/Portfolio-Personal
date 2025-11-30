084a405f12aef5e3d7d23be16928f692cccef6be
F1r0x <patrick94schonewolf@gmail.com>
2025-11-30 10:58:36 +0100

    prueba 12

--- next.config.js
+++ next.config.js
@@ -1,31 +1,6 @@
-// src/config/index.js
-export const email = 'patrickschonewolf@hotmail.com';
-export const featuredProjects = featuredProjectsData;
-export const projects = projectsData;
-
-export const skills = [
-  'Linux', 'HTML', 'CSS', 'JavaScript', 'React', 'Next.js', 'Bash',
-  'TypeScript', 'Git', 'Docker', 'Virtualbox', 'Nmap', 'Metasploit',
-  'Wireshark', 'Burp Suite', 'WordPress', 'Elementor', 'MySQL', 'N8N',
-  'OpenAI API', 'Photoshop',
-];
-
-export const socialMedia = [
-  { name: 'GitHub', url: 'https://github.com/f1r0x' },
-  { name: 'Linkedin', url: 'https://www.linkedin.com/in/f1r0x/' },
-];
-
-export const navLinks = [
-  { name: 'Sobre Mí', url: '/#about' },
-  { name: 'Proyectos', url: '/#projects' },
-  { name: 'Experiencia', url: '/#experience' },
-  { name: 'Estudios', url: '/#education' },
-  { name: 'HTB', url: '/#hackthebox' },
-  { name: 'Contacto', url: '/#contact' },
-];
-
-export const colors = {
-  green: '#64ffda',
-  navy: '#0a192f',
-  darkNavy: '#020c1b',
+module.exports = {
+  images: {
+    sizes: [320, 480, 820, 1200],
+    domains: ['jrgarciadev.s3.amazonaws.com'],
+  },
 };
